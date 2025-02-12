5913ad545dc1d0bcf9af2696460d82e10bdcd24a
Деян <deo.ant.ivanov@gmail.com>
2025-02-12 20:51:59 +0200

    Условна компилация и вмъкване на заглавни файлове

---
+++ 05.11/school.c
@@ -0,0 +1,12 @@
+#include "school_class.h"
+#include "school_room.h"
+
+typedef struct {
+    SchoolClass classes[20];
+    SchoolRoom rooms[10];
+} School;
+
+int main() {
+    School school;
+    return 0;
+}
