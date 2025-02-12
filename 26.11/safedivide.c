b23d385ca7f9860263324c12946d1442038d9631
Деян <deo.ant.ivanov@gmail.com>
2025-02-12 21:10:40 +0200

    Разполагане на данни в паметта

---
+++ 26.11/safedivide.c
@@ -0,0 +1,30 @@
+#include "safeint.h"
+#include "util.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        print_error("Usage: safedivide <num1> <num2>");
+        return 1;
+    }
+
+    SafeResult num1 = safestrtoint(argv[1]);
+    SafeResult num2 = safestrtoint(argv[2]);
+
+    if (num1.errorflag || num2.errorflag) {
+        print_error("Invalid input.");
+        return 1;
+    }
+
+    SafeResult result = safedivide(num1.value, num2.value);
+
+    if (result.errorflag) {
+        print_error("Division by zero or overflow occurred.");
+        return 1;
+    }
+
+    printf("Result: %d\n", result.value);
+    return 0;
+}
+
+
